e3d7a984f706a966f4cbc674f6e323861742b0f0
Lars Francke <github@lars-francke.de>
2021-02-04 14:56:45 +0100

    Updates kube-rs to latest version (#46)
    This requires some breaking API changes.
    I've removed a method that would have required a change but that isn't needed anymore.

--- src/controller.rs
+++ src/controller.rs
@@ -1,3 +1,118 @@
+//! A way to implement a Kubernetes Operator on top of the [kube-rs](https://github.com/clux/kube-rs) library.
+//!
+//! This is an opinionated wrapper around the [`kube_runtime::Controller`] from said library.
+//! The idea is that a single reconcile run can _and should_ be separated into lots of small steps
+//! each taking the reconciliation one step towards a stable state.
+//!
+//! Every time the Operator changes any visible state in Kubernetes it should immediately return and requeue the resource.
+//! The _requeue_ can happen immediately or after a custom duration.
+//! The latter can be useful for operations that are expected to take a while (e.g. creating a resource which is under control by a different Operator).
+//!
+//! This module has a [`Controller`] which is the main object that drives the reconciliation loop.
+//! Custom logic needs to be plugged in by implementing a [`ControllerStrategy`] trait.
+//! The `Controller` will call `init_reconcile_state(...)` on the `ControllerStrategy` for every reconciliation that is triggered.
+//! This method returns a `ReconciliationState` trait which can contain arbitrary state about this
+//! specific reconciliation run.
+//!
+//! Once the state object has been created its `reconcile` method is called.
+//! The `reconcile` method is where the business logic for each operator gets called.
+//! `ReconcileFunctionAction`s can be used to chain multiple calls to separate methods.
+//! By using the `then` method on `ReconcileFunctionAction` we can automatically abort and/or requeue
+//! in case of an `Error` or a `ReconciliationFunctionAction::Requeue(duration)`.
+//!
+//! See the example below for how to use this abstraction in a real-world operator.
+//!
+//! # Example
+//!
+//! ```no_run
+//! use kube::Api;
+//! use k8s_openapi::api::core::v1::Pod;
+//! use stackable_operator::client;
+//! use stackable_operator::controller::{Controller, ControllerStrategy, ReconciliationState};
+//! use stackable_operator::reconcile::{ReconciliationContext, ReconcileFunctionAction, ReconcileResult};
+//! use std::pin::Pin;
+//! use std::future::Future;
+//! use std::time::Duration;
+//!
+//! struct FooStrategy {
+//! }
+//!
+//! struct FooState {
+//!     my_state: i32
+//! }
+//!
+//! type FooReconcileResult = ReconcileResult<String>;
+//!
+//! impl FooState {
+//!     async fn test1(&mut self) -> FooReconcileResult {
+//!         self.my_state = 123;
+//!         println!("My reconciliation logic part 1");
+//!         Ok(ReconcileFunctionAction::Continue)
+//!     }
+//!
+//!     async fn test2(&self) -> FooReconcileResult {
+//! println!("My reconciliation logic part 2");
+//!         if self.my_state > 100 {
+//!             return Ok(ReconcileFunctionAction::Requeue(Duration::from_secs(10)));
+//!         }   
+//!         Ok(ReconcileFunctionAction::Continue)
+//!     }
+//!
+//!     async fn test3(&self) -> FooReconcileResult {
+//! println!("My reconciliation logic part 3");
+//!         if self.my_state > 100 {
+//!             return Ok(ReconcileFunctionAction::Done);
+//!         }   
+//!         Ok(ReconcileFunctionAction::Continue)
+//!     }
+//! }
+//!
+//! impl ReconciliationState for FooState {
+//!     type Error = String;
+//!
+//!     fn reconcile(
+//!         &mut self,
+//!     ) -> Pin<Box<dyn Future<Output = Result<ReconcileFunctionAction, Self::Error>> + Send + '_>>
+//!     {
+//!         Box::pin(async move {
+//!             self.test1()
+//!                 .await?
+//!                 .then(self.test2())
+//!                 .await?
+//!                 .then(self.test3())
+//!                 .await
+//!         })
+//!     }
+//!     
+//! }
+//!
+//! impl ControllerStrategy for FooStrategy {
+//!     type Item = Pod;
+//!     type State = FooState;
+//!
+//!     fn finalizer_name(&self) -> String {
+//!         "foo.stackable.de/finalizer".to_string()
+//!     }
+//!
+//!     fn init_reconcile_state(&self,context: ReconciliationContext<Self::Item>) -> Self::State {
+//!         FooState {
+//!             my_state: 1
+//!         }
+//!     }     
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let client = client::create_client(None).await.unwrap();
+//!     let pods_api: Api<Pod> = client.get_all_api();
+//!
+//!     let controller = Controller::new(pods_api);
+//!
+//!     let strategy = FooStrategy {};
+//!     controller.run(client, strategy).await;
+//! }
+//! ```
+//!
 use crate::client::Client;
 use crate::error::{Error, OperatorResult};
 use crate::reconcile::{ReconcileFunctionAction, ReconciliationContext};
@@ -15,16 +130,15 @@ use std::pin::Pin;
 use std::time::Duration;
 use tracing::{debug, error, info, trace};
 
-/// Every operator needs to provide an implementation of this trait as it provides the operator specific logic.
+/// Every operator needs to provide an implementation of this trait as it provides the operator specific business logic.
 pub trait ControllerStrategy {
     type Item;
     type State: ReconciliationState;
 
     fn finalizer_name(&self) -> String;
 
+    // TODO: Pass in error
     fn error_policy(&self) -> ReconcilerAction {
-        // TODO: Pass in error
-        // TODO: return ReconcilerAction?
         error!("Reconciliation error");
         reconcile::create_requeuing_reconciler_action(Duration::from_secs(30))
     }
@@ -46,17 +160,21 @@ pub trait ReconciliationState {
 
     // The anonymous lifetime refers to the &self. So we could also rewrite this function signature
     // as `fn reconcile_operations<'a>(&'a self, .... >> + 'a>>>;` but that'd require every implementor
-    // to also write all the lifetimes.
-    // Just using the anonymous one makes it a bit easier.
-    // Choosing this lifetime instead of 'static was deliberate because it allows us to return Futures
-    // that take a `self` argument and the Controller is the owner of the `ReconciliationState` object
-    // so this should work just fine.
+    // to also write out all the lifetimes.
+    // Just using the anonymous one makes it a bit easier to read albeit less explicit.
     //
-    /// Provides a list of futures all taking no arguments and returning a [`ReconcileFunctionAction`].
-    /// The controller will call them in order until one of them does _not_ return `Continue`.
-    fn reconcile_operations(
-        &self,
-    ) -> Vec<Pin<Box<dyn Future<Output = Result<ReconcileFunctionAction, Self::Error>> + '_>>>;
+    // Choosing this anonymous lifetime instead of 'static was deliberate.
+    // It ties the lifetime of the returned Future to the `self` that we pass in.
+    // This is often not desired and `static is easier to use because then the returned Future is fully owned by the caller.
+    // In this case the `self` (i.e. the `ReconciliationState`) is fully owned by the caller (the `Controller`) and it never escapes.
+    //
+    // TODO: I'm not sure but can we maybe use async-trait and just make this function async and return the Result<...> directly?
+    //
+    /// Returns a Future that - when completed - will either return an `Error` or a `ReconciliationFunctionAction`.
+    /// The return result can be used to requeue the same resource for later.
+    fn reconcile(
+        &mut self,
+    ) -> Pin<Box<dyn Future<Output = Result<ReconcileFunctionAction, Self::Error>> + Send + '_>>;
 }
 
 /// A Controller is the object that watches all required resources and runs the reconciliation loop.
@@ -116,7 +234,8 @@ where
     /// It'll start talking to Kubernetes and will call the `Strategy` implementation.
     pub async fn run<S>(self, client: Client, strategy: S)
     where
-        S: ControllerStrategy<Item = T> + 'static,
+        S: ControllerStrategy<Item = T> + Send + Sync + 'static,
+        S::State: Send,
     {
         let context = Context::new(ControllerContext { client, strategy });
 
@@ -166,33 +285,24 @@ where
 
     let rc = ReconciliationContext::new(context.client.clone(), resource.clone());
 
-    let state = strategy.init_reconcile_state(rc);
-    let futures = state.reconcile_operations();
-
-    for future in futures {
-        let result = future.await;
-
-        match result {
-            Ok(ReconcileFunctionAction::Continue) => {
-                trace!("Reconciler loop: Continue");
-            }
-            Ok(ReconcileFunctionAction::Done) => {
-                trace!("Reconciler loop: Done");
-                break;
-            }
-            Ok(ReconcileFunctionAction::Requeue(duration)) => {
-                trace!(?duration, "Reconciler loop: Requeue");
-                return Ok(ReconcilerAction {
-                    requeue_after: Some(duration),
-                });
-            }
-            Err(err) => {
-                error!(?err, "Error reconciling");
-                return Ok(reconcile::create_requeuing_reconciler_action(
-                    Duration::from_secs(30),
-                ));
-                // TODO: Make configurable
-            }
+    let mut state = strategy.init_reconcile_state(rc);
+    let result = state.reconcile().await;
+    match result {
+        Ok(ReconcileFunctionAction::Requeue(duration)) => {
+            trace!(?duration, "Reconciler loop: Requeue");
+            return Ok(ReconcilerAction {
+                requeue_after: Some(duration),
+            });
+        }
+        Ok(action) => {
+            trace!("Reconciler loop: {:?}", action);
+        }
+        Err(err) => {
+            error!("Error reconciling [{:?}]", err);
+            return Ok(ReconcilerAction {
+                // TODO: Make this configurable
+                requeue_after: Some(Duration::from_secs(30)),
+            });
         }
     }
 
