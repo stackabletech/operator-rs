62dee98e8bf89650280491047c0ddb1d557837be
maltesander <malte.sander.it@gmail.com>
2021-04-08 16:22:12 +0200

    Added automatic support for app.kubernetes.io/instance label in build_metadata.

--- src/metadata.rs
+++ src/metadata.rs
@@ -5,12 +5,26 @@ use k8s_openapi::Resource;
 use kube::api::{Meta, ObjectMeta};
 use std::collections::BTreeMap;
 
+/// The name of the application	e.g. "mysql"
+pub const APP_KUBERNETES_IO_NAME: &str = "app.kubernetes.io/name";
+/// A unique name identifying the instance of an application e.g. "mysql-abcxzy"
+pub const APP_KUBERNETES_IO_INSTANCE: &str = "app.kubernetes.io/instance";
+/// The current version of the application (e.g., a semantic version, revision hash, etc.) e.g."5.7.21"
+pub const APP_KUBERNETES_IO_VERSION: &str = "app.kubernetes.io/version";
+///	The component within the architecture e.g. database
+pub const APP_KUBERNETES_IO_COMPONENT: &str = "app.kubernetes.io/component";
+/// The name of a higher level application this one is part of e.g. "wordpress"
+pub const APP_KUBERNETES_IO_PART_OF: &str = "app.kubernetes.io/part-of";
+/// The tool being used to manage the operation of an application e.g. helm
+pub const APP_KUBERNETES_IO_MANAGED_BY: &str = "app.kubernetes.io/managed-by";
+
 /// Builds a `ObjectMeta` object out of a template/owner object.
 ///
 /// Automatically sets:
 /// * name
 /// * namespace (if the object passed in had one)
 /// * labels (if provided)
+/// * kubernetes recommended labels e.g. app.kubernetes.io/instance
 /// * ownerReferences (pointing at the object that was passed in).
 pub fn build_metadata<T>(
     name: String,
@@ -21,8 +35,14 @@ pub fn build_metadata<T>(
 where
     T: Meta,
 {
+    let mut merged_labels = get_recommended_labels(resource)?;
+
+    if let Some(provided_labels) = labels {
+        merged_labels.extend(provided_labels);
+    }
+
     Ok(ObjectMeta {
-        labels,
+        labels: Some(merged_labels),
         name: Some(name),
         namespace: Meta::namespace(resource),
         owner_references: Some(vec![object_to_owner_reference::<T>(
@@ -31,6 +51,27 @@ where
         )?]),
         ..ObjectMeta::default()
     })
+}
+
+/// Create the kubernetes recommended labels:
+/// - app.kubernetes.io/name - The name of the application	e.g. mysql
+/// - app.kubernetes.io/instance - A unique name identifying the instance of an application e.g. mysql-abcxzy
+/// - app.kubernetes.io/version	- The current version of the application (e.g., a semantic version, revision hash, etc.) e.g. 5.7.21
+/// - app.kubernetes.io/component - The component within the architecture e.g. database
+/// - app.kubernetes.io/part-of	- The name of a higher level application this one is part of e.g. wordpress
+/// - app.kubernetes.io/managed-by - The tool being used to manage the operation of an application e.g. helm
+///
+fn get_recommended_labels<T>(resource: &T) -> OperatorResult<BTreeMap<String, String>>
+where
+    T: Meta,
+{
+    let mut recommended_labels = BTreeMap::new();
+    // TODO: throw error if no name found? Can that even happen?
+    if let Some(name) = &resource.meta().name {
+        recommended_labels.insert(APP_KUBERNETES_IO_INSTANCE.to_string(), name.clone());
+    }
+
+    Ok(recommended_labels)
 }
 
 /// Creates an OwnerReference pointing to the resource type and `metadata` being passed in.
