362bdb3094dd7f52e0b0494dee2a9674de9280c2
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2021-08-13 15:50:44 +0200

    Second attempt at setting the default log level to INFO. Fixes #189

--- src/logging.rs
+++ src/logging.rs
@@ -1,4 +1,4 @@
-use tracing_subscriber::filter::LevelFilter;
+use tracing;
 use tracing_subscriber::EnvFilter;
 
 /// Initializes `tracing` logging with options from the environment variable
@@ -8,8 +8,35 @@ use tracing_subscriber::EnvFilter;
 /// We encourage it to be the product name plus `_LOG`, e.g. `FOOBAR_OPERATOR_LOG`.
 /// If no environment variable is provided, the maximum log level is set to INFO.
 pub fn initialize_logging(env: &str) {
-    tracing_subscriber::fmt()
-        .with_max_level(LevelFilter::INFO)
-        .with_env_filter(EnvFilter::from_env(env))
-        .init();
+    let filter = match EnvFilter::try_from_env(env) {
+        Ok(env_filter) => env_filter,
+        _ => EnvFilter::try_new(tracing::Level::INFO.to_string())
+            .expect("Failed to initialize default tracing level to INFO"),
+    };
+
+    tracing_subscriber::fmt().with_env_filter(filter).init();
+}
+
+#[cfg(test)]
+mod test {
+
+    use tracing::{debug, error, info};
+
+    // If there is a proper way to programmatically inspect the global max level than we should use that.
+    // Until then, this is mostly a sanity check for the implementation above.
+    // Either run
+    //      cargo test default_tracing -- --nocapture
+    // to see the ERROR and INFO messages, or
+    //      NOT_SET=debug cargo test default_tracing -- --nocapture
+    // to see them all.
+    #[test]
+    pub fn test_default_tracing_level_is_set_to_info() {
+        super::initialize_logging("NOT_SET");
+
+        error!("ERROR level messages should be seen.");
+        info!("INFO level messages should also be seen by default.");
+        debug!("DEBUG level messages should be seen only if you set the NOT_SET env var.");
+
+        assert!(true);
+    }
 }
