3251f7585d20096730ef40feb5f7cf6e425c08ae
Lars Francke <lars.francke@gmail.com>
2021-08-13 23:55:49 +0200

    documentation

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,32 @@
 use tracing::info;
 
+/// Prints helpful and standardized diagnostic messages.
+///
+/// This method is meant to be called first thing in the `main` method of an Operator.
+///
+/// # Usage
+///
+/// Use the [`built`](https://crates.io/crates/built) crate and include it in your `main.rs` like this:
+///
+/// ```ignore
+/// mod built_info {
+///     // The file has been placed there by the build script.
+///     include!(concat!(env!("OUT_DIR"), "/built.rs"));
+/// }
+/// ```
+///
+/// Then call this method in your `main` method:
+///
+/// ```ignore
+/// stackable_operator::utils::print_startup_string(
+///      built_info::PKG_DESCRIPTION,
+///      built_info::PKG_VERSION,
+///      built_info::GIT_VERSION,
+///      built_info::TARGET,
+///      built_info::BUILT_TIME_UTC,
+///      built_info::RUSTC_VERSION,
+/// );
+/// ```
 pub fn print_startup_string(
     pkg_description: &str,
     pkg_version: &str,
