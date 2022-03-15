9d0fcea210017affcf82111d2f5757c7a73d4639
Malte Sander <malte.sander.it@gmail.com>
2022-03-15 17:01:43 +0100

    adapted to PR review

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -8,44 +8,32 @@ pub mod event;
 pub mod meta;
 pub mod pod;
 
-#[deprecated(
-    since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::configmap::*` in the future"
-)]
+#[deprecated(since = "0.15.0", note = "Please use `builder::configmap::*` instead.")]
 pub use configmap::*;
 
-#[deprecated(
-    since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::event::*` in the future"
-)]
+#[deprecated(since = "0.15.0", note = "Please use `builder::event::*` instead.")]
 pub use event::*;
 
-#[deprecated(
-    since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::meta::*` in the future"
-)]
+#[deprecated(since = "0.15.0", note = "Please use `builder::meta::*` instead.")]
 pub use meta::*;
 
 #[deprecated(
     since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::pod::container::*` in the future"
+    note = "Please use `builder::pod::container::*` instead."
 )]
 pub use pod::container::*;
 
 #[deprecated(
     since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::pod::security::*` in the future"
+    note = "Please use `builder::pod::security::*` instead."
 )]
 pub use pod::security::*;
 
 #[deprecated(
     since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::pod::volume::*` in the future"
+    note = "Please use `builder::pod::volume::*` instead."
 )]
 pub use pod::volume::*;
 
-#[deprecated(
-    since = "0.15.0",
-    note = "This is for compatibility. Please use `builder::pod::*` in the future"
-)]
+#[deprecated(since = "0.15.0", note = "Please use `builder::pod::*` instead.")]
 pub use pod::*;
