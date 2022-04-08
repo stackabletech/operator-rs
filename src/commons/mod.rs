accd6f21b66b156dc4d1d2a1d65a115a3c73419d
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-04-08 16:19:15 +0200

    fix module visibility

--- src/commons/mod.rs
+++ src/commons/mod.rs
@@ -1,6 +1,6 @@
 //! This module provides common datastructures or CRDs shared between all the operators
 
 pub mod authentication;
-mod ldap;
-mod secret_class;
+pub mod ldap;
+pub mod secret_class;
 pub mod tls;
