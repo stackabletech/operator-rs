5e7265f36a3212d979b6abff69bf3e55b6914ed9
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2022-05-10 15:16:14 +0200

    Cleanup and consolidation

--- src/utils.rs
+++ src/utils.rs
@@ -75,35 +75,57 @@ impl BinaryMultiple {
         match self {
             BinaryMultiple::Kibi => BinaryMultiple::Kibi,
             BinaryMultiple::Mebi => BinaryMultiple::Kibi,
-            BinaryMultiple::Gibi => BinaryMultiple::Mebi, 
-            BinaryMultiple::Tebi => BinaryMultiple::Gibi, 
-            BinaryMultiple::Pebi => BinaryMultiple::Tebi, 
-            BinaryMultiple::Exbi => BinaryMultiple::Pebi, 
+            BinaryMultiple::Gibi => BinaryMultiple::Mebi,
+            BinaryMultiple::Tebi => BinaryMultiple::Gibi,
+            BinaryMultiple::Pebi => BinaryMultiple::Tebi,
+            BinaryMultiple::Exbi => BinaryMultiple::Pebi,
+        }
+    }
+}
+
+impl FromStr for BinaryMultiple {
+    type Err = Error;
+
+    fn from_str(q: &str) -> OperatorResult<BinaryMultiple> {
+        let lq = q.to_lowercase();
+        match lq.as_str() {
+            "ki" | "kib" => Ok(BinaryMultiple::Kibi),
+            "mi" | "mib" => Ok(BinaryMultiple::Mebi),
+            "gi" | "gib" => Ok(BinaryMultiple::Gibi),
+            "ti" | "tib" => Ok(BinaryMultiple::Tebi),
+            "pi" | "pib" => Ok(BinaryMultiple::Pebi),
+            "ei" | "eib" => Ok(BinaryMultiple::Exbi),
+            _ => Err(Error::InvalidQuantityUnit {
+                value: q.to_string(),
+            }),
         }
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
-struct QuantityAndUnit {
+pub struct QuantityAndUnit {
     value: f32,
     unit: BinaryMultiple,
 }
 
 impl QuantityAndUnit {
-
     pub fn scale(&self, factor: f32) -> Self {
-        if factor < 1.0 {
+        if factor < 1.0 && self.unit != BinaryMultiple::Kibi {
             QuantityAndUnit {
                 value: self.value * factor * 1024.0,
                 unit: self.unit.upscale(),
             }
-        }
-        else {
+        } else {
             QuantityAndUnit {
                 value: self.value * factor,
                 unit: self.unit.clone(),
             }
-        } 
+        }
+    }
+
+    fn to_java_heap(&self, factor: f32) -> String {
+        let scaled = self.scale(factor);
+        format!("-Xmx{:.0}{}", scaled.value, scaled.unit.to_legacy())
     }
 }
 
@@ -125,34 +147,8 @@ impl FromStr for QuantityAndUnit {
             value: v.parse::<f32>().map_err(|_| Error::InvalidQuantity {
                 value: q.to_owned(),
             })?,
-            unit: parse_unit(&u)?,
+            unit: u.parse()?,
         })
-    }
-}
-
-fn parse_unit(q: &str) -> OperatorResult<BinaryMultiple> {
-    let lq = q.to_lowercase();
-    match lq.as_str() {
-        "ki" | "kib" => Ok(BinaryMultiple::Kibi),
-        "mi" | "mib" => Ok(BinaryMultiple::Mebi),
-        "gi" | "gib" => Ok(BinaryMultiple::Gibi),
-        "ti" | "tib" => Ok(BinaryMultiple::Tebi),
-        "pi" | "pib" => Ok(BinaryMultiple::Pebi),
-        "ei" | "eib" => Ok(BinaryMultiple::Exbi),
-        _ => Err(Error::InvalidQuantityUnit {
-            value: q.to_string(),
-        }),
-    }
-}
-
-pub trait ToJavaHeap {
-    fn to_java_heap(&self, factor: f32) -> String;
-}
-
-impl ToJavaHeap for QuantityAndUnit {
-    fn to_java_heap(&self, factor: f32) -> String {
-        let scaled = self.scale(factor);
-        format!("-Xmx{:.0}{}", scaled.value, scaled.unit.to_legacy())
     }
 }
 
@@ -161,23 +157,27 @@ mod test {
     use k8s_openapi::apimachinery::pkg::api::resource::Quantity;
 
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    pub fn test_parse() {
-        let got = "1.5Gi".parse().unwrap();
-        assert_eq!(
-            QuantityAndUnit {
-                value: 1.5f32,
-                unit: BinaryMultiple::Gibi
-            },
-            got
-        );
+    #[rstest]
+    #[case("256ki", QuantityAndUnit { value: 256f32, unit: BinaryMultiple::Kibi })]
+    #[case("8Mib", QuantityAndUnit { value: 8f32, unit: BinaryMultiple::Mebi })]
+    #[case("1.5Gi", QuantityAndUnit { value: 1.5f32, unit: BinaryMultiple::Gibi })]
+    #[case("0.8tib", QuantityAndUnit { value: 0.8f32, unit: BinaryMultiple::Tebi })]
+    #[case("3.2Pi", QuantityAndUnit { value: 3.2f32, unit: BinaryMultiple::Pebi })]
+    #[case("0.2ei", QuantityAndUnit { value: 0.2f32, unit: BinaryMultiple::Exbi })]
+    pub fn test_quantity_parse(#[case] input: &str, #[case] output: QuantityAndUnit) {
+        let got = input.parse::<QuantityAndUnit>().unwrap();
+        assert_eq!(got, output);
     }
 
-
-    #[test]
-    pub fn test_q_to_qu() {
-        let qu: QuantityAndUnit = Quantity("2Gi".to_owned()).0.parse().unwrap();
-        assert_eq!("-Xmx1638m", qu.to_java_heap(0.8));
+    #[rstest]
+    #[case("256ki", 1.0, "-Xmx256k")]
+    #[case("256ki", 0.8, "-Xmx205k")]
+    #[case("2mib", 0.8, "-Xmx1638k")]
+    #[case("1.5GiB", 0.8, "-Xmx1229m")]
+    pub fn test_quantity_scale(#[case] q: &str, #[case] factor: f32, #[case] heap: &str) {
+        let qu: QuantityAndUnit = Quantity(q.to_owned()).0.parse().unwrap();
+        assert_eq!(heap, qu.to_java_heap(factor));
     }
 }
