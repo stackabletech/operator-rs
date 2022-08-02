8fe6886acb1a4835270dbeb2f0ad29b2b3b682ed
Teo Klestrup Röijezon <teo@nullable.se>
2022-08-02 15:50:14 +0000

    Expose `Client::get_opt` using `Api::get_opt` (#451)
    ## Description
    
    *Please add a description here. This will become the commit message of the merge request later.*
    
    
    
    Co-authored-by: Teo Klestrup Röijezon <teo.roijezon@stackable.de>

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use crate::error::{Error, OperatorResult};
+use crate::error::OperatorResult;
 use crate::label_selector;
 
 use backoff::backoff::Backoff;
@@ -9,7 +9,6 @@ use k8s_openapi::apimachinery::pkg::apis::meta::v1::LabelSelector;
 use kube::api::{DeleteParams, ListParams, Patch, PatchParams, PostParams, Resource, ResourceExt};
 use kube::client::Client as KubeClient;
 use kube::core::Status;
-use kube::error::ErrorResponse;
 use kube::runtime::WatchStreamExt;
 use kube::{Api, Config};
 use serde::de::DeserializeOwned;
@@ -78,12 +77,26 @@ impl Client {
         Ok(self.get_api(namespace).get(resource_name).await?)
     }
 
+    /// Retrieves a single instance of the requested resource type with the given name, if it exists.
+    pub async fn get_opt<T>(
+        &self,
+        resource_name: &str,
+        namespace: Option<&str>,
+    ) -> OperatorResult<Option<T>>
+    where
+        T: Clone + Debug + DeserializeOwned + Resource,
+        <T as Resource>::DynamicType: Default,
+    {
+        Ok(self.get_api(namespace).get_opt(resource_name).await?)
+    }
+
     /// Returns Ok(true) if the resource has been registered in Kubernetes, Ok(false) if it could
     /// not be found and Error in any other case (e.g. connection to Kubernetes failed in some way).
     /// Kubernetes does not offer a pure exists check. Therefore we currently use the get() method
     /// and ignore the (in case of existing) returned resource. We should replace this with a pure
     /// exists method as soon as it becomes available (e.g. only returning Ok/Success) to reduce
     /// network traffic.
+    #[deprecated(since = "0.24.0", note = "Replaced by `get_opt`")]
     pub async fn exists<T>(
         &self,
         resource_name: &str,
@@ -93,14 +106,9 @@ impl Client {
         T: Clone + Debug + DeserializeOwned + Resource,
         <T as Resource>::DynamicType: Default,
     {
-        let resource: OperatorResult<T> = self.get(resource_name, namespace).await;
-        match resource {
-            Ok(_) => Ok(true),
-            Err(Error::KubeError {
-                source: kube::error::Error::Api(ErrorResponse { reason, .. }),
-            }) if reason == "NotFound" => Ok(false),
-            Err(err) => Err(err),
-        }
+        self.get_opt::<T>(resource_name, namespace)
+            .await
+            .map(|obj| obj.is_some())
     }
 
     /// Retrieves all instances of the requested resource type.
@@ -373,9 +381,10 @@ impl Client {
         self.delete(&resource).await?;
 
         loop {
-            if !self
-                .exists::<T>(&resource.name_any(), resource.namespace().as_deref())
+            if self
+                .get_opt::<T>(&resource.name_any(), resource.namespace().as_deref())
                 .await?
+                .is_none()
             {
                 return Ok(());
             }
