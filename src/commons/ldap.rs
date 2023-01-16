4dba6f682bb191f5fc807bab50eb0b92d7bb63fb
Felix Hennig <sebastian.bernauer@stackable.de>
2023-01-16 10:56:34 +0000

    Feat: LDAP helper functions (#535)
    ## Description
    
    closes #534 
    
    
    
    Co-authored-by: Felix Hennig <felix.hennig@stackable.tech>
    Co-authored-by: Felix Hennig <fhennig@users.noreply.github.com>

--- src/commons/ldap.rs
+++ src/commons/ldap.rs
@@ -1,7 +1,12 @@
-use crate::commons::secret_class::SecretClassVolume;
+use crate::builder::ContainerBuilder;
 use crate::commons::tls::Tls;
+use crate::{builder::PodBuilder, commons::secret_class::SecretClassVolume};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+
+use super::tls::{CaCert, TlsServerVerification, TlsVerification};
+
+pub const SECRET_BASE_PATH: &str = "/stackable/secrets";
 
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -30,6 +35,95 @@ impl LdapAuthenticationProvider {
         match self.tls {
             None => 389,
             Some(_) => 636,
+        }
+    }
+
+    /// This functions adds
+    ///
+    /// * The needed volumes to the PodBuilder
+    /// * The needed volume_mounts to all the ContainerBuilder in the list (e.g. init + main container)
+    ///
+    /// This function will handle
+    ///
+    /// * Bind credentials needed to connect to LDAP server
+    /// * Tls secret class used to verify the cert of the LDAP server
+    pub fn add_volumes_and_mounts(
+        &self,
+        pod_builder: &mut PodBuilder,
+        container_builders: Vec<&mut ContainerBuilder>,
+    ) {
+        let mut mounts: Vec<(String, String)> = Vec::new();
+        if let Some(bind_credentials) = &self.bind_credentials {
+            let secret_class = bind_credentials.secret_class.to_owned();
+            let volume_name = format!("{secret_class}-bind-credentials");
+
+            pod_builder.add_volume(bind_credentials.to_volume(&volume_name));
+            mounts.push((volume_name, secret_class));
+        }
+        if let Some(secret_class) = self.tls_ca_cert_secret_class() {
+            let volume_name = format!("{secret_class}-ca-cert");
+            let volume = SecretClassVolume {
+                secret_class: secret_class.to_string(),
+                scope: None,
+            }
+            .to_volume(&volume_name);
+
+            pod_builder.add_volume(volume);
+            mounts.push((volume_name, secret_class));
+        }
+        for cb in container_builders {
+            for (mount, secret_class) in mounts.iter() {
+                cb.add_volume_mount(mount, format!("{SECRET_BASE_PATH}/{secret_class}"));
+            }
+        }
+    }
+
+    /// Returns the path of the files containing bind user and password.
+    /// This will be None if there are no credentials for this LDAP connection.
+    pub fn bind_credentials_mount_paths(&self) -> Option<(String, String)> {
+        self.bind_credentials.as_ref().map(|bind_credentials| {
+            let secret_class = &bind_credentials.secret_class;
+            (
+                format!("{SECRET_BASE_PATH}/{secret_class}/user"),
+                format!("{SECRET_BASE_PATH}/{secret_class}/password"),
+            )
+        })
+    }
+
+    /// Whether TLS is configured
+    pub fn use_tls(&self) -> bool {
+        self.tls.is_some()
+    }
+
+    /// Whether TLS verification is configured
+    /// Returns false if TLS itself isn't configured
+    pub fn use_tls_verification(&self) -> bool {
+        if let Some(tls) = &self.tls {
+            tls.verification != TlsVerification::None {}
+        } else {
+            false
+        }
+    }
+
+    /// Returns the path of the ca.crt that should be used to verify the LDAP server certificate
+    /// if TLS verification with a CA cert from a SecretClass is configured.
+    pub fn tls_ca_cert_mount_path(&self) -> Option<String> {
+        self.tls_ca_cert_secret_class()
+            .map(|secret_class| format!("{SECRET_BASE_PATH}/{secret_class}/ca.crt"))
+    }
+
+    /// Extracts the secret class that provides the CA used to verify the LDAP server certificate.
+    fn tls_ca_cert_secret_class(&self) -> Option<String> {
+        if let Some(Tls {
+            verification:
+                TlsVerification::Server(TlsServerVerification {
+                    ca_cert: CaCert::SecretClass(secret_class),
+                }),
+        }) = &self.tls
+        {
+            Some(secret_class.to_owned())
+        } else {
+            None
         }
     }
 }
