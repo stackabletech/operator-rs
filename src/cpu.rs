6f4bb27dcbddcbfacc8f7db50d7d7661478b3992
Felix Hennig <felix.hennig@stackable.tech>
2023-01-30 10:35:11 +0100

    docs: Documented CpuQuantity::from_str

--- src/cpu.rs
+++ src/cpu.rs
@@ -31,6 +31,12 @@ impl CpuQuantity {
 impl FromStr for CpuQuantity {
     type Err = Error;
 
+    /// Only two formats can be parsed
+    /// - <usize>m
+    /// - <f32>
+    /// For the float, only milli-precision is supported.
+    /// Using more precise values will trigger an error, and using any other
+    /// unit than 'm' or None will also trigger an error.
     fn from_str(q: &str) -> OperatorResult<Self> {
         let start_of_unit = q.find(|c: char| c != '.' && !c.is_numeric());
         if let Some(start_of_unit) = start_of_unit {
