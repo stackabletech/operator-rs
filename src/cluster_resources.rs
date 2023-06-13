b4da44e648b77babd9c9cc40523bac2506dbdb36
Techassi <git@techassi.dev>
2023-06-13 09:49:39 +0200

    Update src/cluster_resources.rs
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- src/cluster_resources.rs
+++ src/cluster_resources.rs
@@ -187,8 +187,10 @@ impl ClusterResource for StatefulSet {
         }
     }
 
-    fn pod_spec(&self) -> Option<PodSpec> {
-        self.spec.clone().unwrap_or_default().template.spec
+    fn pod_spec(&self) -> Option<&PodSpec> {
+        self.spec
+            .as_ref()
+            .and_then(|spec| spec.template.spec.as_ref())
     }
 }
 
