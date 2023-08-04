cf8143a640d689d5f26ed3f7610827a089002e09
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-08-04 18:08:57 +0300

    Some cleanup.

--- src/jvm/mod.rs
+++ src/jvm/mod.rs
@@ -1,3 +1,5 @@
+//! JVM configuration management.
+//! Currently it supports only JVM security properties.
 use std::collections::{BTreeMap, HashMap};
 
 use k8s_openapi::api::core::v1::ConfigMap;
@@ -12,28 +14,34 @@ use crate::{
     error::OperatorResult,
 };
 
-/// JVM configuration management.
-
+/// Java system property that points to the location of the custom security configuration file
 pub const SECURITY_SYSTEM_PROPERTY_NAME: &str = "java.security.properties";
+/// Name of the custom security configuration file
 pub const SECURITY_FILE_NAME: &str = "security.properties";
 
-// This is a preliminary interface. Operators should be ignorant to the actual structure.
+/// Seconds to cache positive DNS results.
+pub const PROP_NAME_NET_ADDR_CACHE_TTL: &str = "networkaddress.cache.ttl";
+/// Seconds to cache negative DNS results.
+pub const PROP_NAME_NET_ADDR_CACHE_NEGATIVE_TTL: &str = "networkaddress.cache.negative.ttl";
+
+/// TODO: This is a preliminary interface. Operators should be ignorant to the actual structure.
+/// Structure that holds Java security properties.
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 pub struct Security {
     properties: HashMap<String, Option<String>>,
 }
 
-// TODO: decide on the defaults here
+/// TODO: decide on the defaults here
 impl Default for Security {
     fn default() -> Self {
         Self {
             properties: vec![
                 (
-                    "networkaddress.cache.ttl".to_string(),
+                    PROP_NAME_NET_ADDR_CACHE_TTL.to_string(),
                     Some("10".to_string()),
                 ),
                 (
-                    "networkaddress.cache.negative.ttl".to_string(),
+                    PROP_NAME_NET_ADDR_CACHE_NEGATIVE_TTL.to_string(),
                     Some("10".to_string()),
                 ),
             ]
@@ -43,11 +51,22 @@ impl Default for Security {
     }
 }
 
+/// Generate a config map for the given Security object.
+///
+/// If no security object is given, the default from this module is used.
+///
+/// The generated config map data contains a single entry with the name and contents
+/// of the custom security configuration file.
 pub fn security_config_map<T: Resource<DynamicType = ()>>(
     owner: &T,
     labels: BTreeMap<String, String>,
-    sec: &Security,
+    security_opt: &Option<Security>,
 ) -> OperatorResult<ConfigMap> {
+    let props = match security_opt {
+        Some(sec) => sec.properties.clone(),
+        _ => Security::default().properties,
+    };
+
     ConfigMapBuilder::new()
         .metadata(
             ObjectMetaBuilder::new()
@@ -59,19 +78,13 @@ pub fn security_config_map<T: Resource<DynamicType = ()>>(
         )
         .add_data(
             SECURITY_FILE_NAME,
-            to_java_properties_string(sec.properties.iter())
+            to_java_properties_string(props.iter())
                 .map_err(|_| Error::JavaProperties(SECURITY_FILE_NAME.to_string()))?,
         )
         .build()
 }
 
-pub fn default_security_config_map<T: Resource<DynamicType = ()>>(
-    owner: &T,
-    labels: BTreeMap<String, String>,
-) -> OperatorResult<ConfigMap> {
-    security_config_map(owner, labels, &Security::default())
-}
-
+/// Java CLI argument for custom security configuration.
 pub fn security_system_property(mountpoint: &str) -> String {
     format!("-D{SECURITY_SYSTEM_PROPERTY_NAME}={mountpoint}/{SECURITY_FILE_NAME}")
 }
