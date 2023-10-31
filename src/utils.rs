fbe4d0c2f666476aaf579e159f856171c2e03f8e
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-31 08:14:01 +0100

    document COMMON_BASH_TRAP_FUNCTIONS

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,28 @@
 use tracing::info;
 
+/// This is a bash snippet, which adds two functions out of interest:
+///
+/// 1. `prepare_signal_handlers` call this first to set up the needed traps
+/// 2. `wait_for_termination` waits for the PID you passed as the first argument to terminate
+///
+/// An example use could be
+/// ```
+/// {COMMON_BASH_TRAP_FUNCTIONS}
+/// echo "Run before startup"
+/// prepare_signal_handlers
+/// {hadoop_home}/bin/hdfs {role} &
+/// wait_for_termination $!
+/// echo "Run after termination"
+/// ```
 pub const COMMON_BASH_TRAP_FUNCTIONS: &str = r#"
 prepare_signal_handlers()
 {
     unset term_child_pid
     unset term_kill_needed
-    trap 'handle_term' TERM
+    trap 'handle_term_signal' TERM
 }
 
-handle_term()
+handle_term_signal()
 {
     if [ "${term_child_pid}" ]; then
         kill -TERM "${term_child_pid}" 2>/dev/null
@@ -20,7 +34,7 @@ handle_term()
 wait_for_termination()
 {
     set +e
-    term_child_pid=$!
+    term_child_pid=$1
     if [[ -v term_kill_needed ]]; then
         kill -TERM "${term_child_pid}" 2>/dev/null
     fi
