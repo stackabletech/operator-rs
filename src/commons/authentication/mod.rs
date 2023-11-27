bd701f7dffea34bc8301f2fc352bc4e18c86fa38
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-11-27 13:39:46 +0100

    Switch oidc from Option<> to an Option<enum>

--- src/commons/authentication/mod.rs
+++ src/commons/authentication/mod.rs
@@ -99,7 +99,19 @@ pub struct ClientAuthenticationDetails<O = ()> {
     /// is flattened into the final CRD.
     ///
     /// Use [`ClientAuthenticationDetails::oidc_or_error`] to get the value or report an error to the user.
-    oidc: Option<oidc::ClientAuthenticationOptions<O>>,
+    ///
+    /// We are aware that this prevents users from e.g. configuring oidc and ldap clientAuthenticationOptions
+    /// simultaneously. This might be helpful in cases of a migration of the provider of an AuthenticationClass,
+    /// but we consider a good validation for 90% of the use-cases more important.
+    #[serde(flatten)]
+    client_authentication_options: Option<ClientAuthenticationOptions<O>>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+
+pub enum ClientAuthenticationOptions<O> {
+    Oidc(oidc::ClientAuthenticationOptions<O>),
 }
 
 impl<O> ClientAuthenticationDetails<O> {
@@ -120,11 +132,12 @@ impl<O> ClientAuthenticationDetails<O> {
         &self,
         auth_class_name: &str,
     ) -> OperatorResult<&oidc::ClientAuthenticationOptions<O>> {
-        self.oidc
-            .as_ref()
-            .ok_or(Error::OidcAuthenticationDetailsNotSpecified {
+        match &self.client_authentication_options {
+            Some(ClientAuthenticationOptions::Oidc(oidc)) => Ok(oidc),
+            None => Err(Error::OidcAuthenticationDetailsNotSpecified {
                 auth_class_name: auth_class_name.to_string(),
-            })
+            }),
+        }
     }
 }
 
