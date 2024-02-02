ea0e20a663ed4c05c02ee932a5e8b92817c07636
Natalie Klestrup Röijezon <teo.roijezon@stackable.de>
2024-02-02 14:29:05 +0100

    Redefine duration math using operators to be consistent
    Some already do this. We should pick one or the other.

--- src/time/duration.rs
+++ src/time/duration.rs
@@ -233,19 +233,19 @@ impl Add<Duration> for std::time::Instant {
     type Output = Self;
 
     fn add(self, rhs: Duration) -> Self::Output {
-        self.add(rhs.0)
+        self + rhs.0
     }
 }
 
 impl AddAssign for Duration {
     fn add_assign(&mut self, rhs: Self) {
-        self.0.add_assign(rhs.0)
+        self.0 += rhs.0
     }
 }
 
 impl AddAssign<Duration> for std::time::Instant {
     fn add_assign(&mut self, rhs: Duration) {
-        self.add_assign(rhs.0)
+        *self += rhs.0
     }
 }
 
@@ -261,19 +261,19 @@ impl Sub<Duration> for std::time::Instant {
     type Output = Self;
 
     fn sub(self, rhs: Duration) -> Self::Output {
-        self.sub(rhs.0)
+        self - rhs.0
     }
 }
 
 impl SubAssign for Duration {
     fn sub_assign(&mut self, rhs: Self) {
-        self.0.sub_assign(rhs.0)
+        self.0 -= rhs.0
     }
 }
 
 impl SubAssign<Duration> for std::time::Instant {
     fn sub_assign(&mut self, rhs: Duration) {
-        self.sub_assign(rhs.0)
+        *self -= rhs.0
     }
 }
 
