bda6e597a7189890241e69d25734e9b9a3d36a10
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-02-06 12:17:11 +0100

    Add initial TLS acceptor

--- stackable-webhook/src/lib.rs
+++ stackable-webhook/src/lib.rs
@@ -6,12 +6,20 @@
 //! The crate is also fully compatible with [`tracing`], and emits multiple
 //! levels of tracing data.
 
-use std::sync::Arc;
+use std::{fs::File, io::BufReader, sync::Arc};
 
-use axum::Router;
+use axum::{extract::Request, Router};
+use futures_util::pin_mut;
+use hyper::body::Incoming;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 use tokio::net::TcpListener;
-use tokio_rustls::rustls::ServerConfig;
-use tracing::{debug, warn};
+use tokio_rustls::{
+    rustls::{Certificate, PrivateKey, ServerConfig},
+    TlsAcceptor,
+};
+use tower::Service;
+use tracing::{debug, error, warn};
 
 pub mod constants;
 pub mod conversion;
@@ -90,12 +98,81 @@ impl WebhookServer {
             }
         }
 
-        // Create the root router and merge the provided router into it.
-        let mut router = Router::new();
-        router = router.merge(self.router);
+        let mut cert_file =
+            &mut BufReader::new(File::open("/tmp/webhook-certs/serverCert.pem").unwrap());
+        let mut key_file =
+            &mut BufReader::new(File::open("/tmp/webhook-certs/serverKey.pem").unwrap());
 
-        let listener = TcpListener::bind(self.options.socket_addr).await.unwrap();
-        axum::serve(listener, router).await.unwrap()
+        let key = PrivateKey(pkcs8_private_keys(&mut key_file).unwrap().remove(0));
+        let certs = certs(&mut cert_file)
+            .unwrap()
+            .into_iter()
+            .map(Certificate)
+            .collect();
+
+        let mut config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(certs, key)
+            .expect("bad certificate/key");
+
+        config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
+        let config = Arc::new(config);
+        let tls_acceptor = TlsAcceptor::from(config);
+
+        // Create the root router and merge the provided router into it.
+        let mut app = Router::new();
+        app = app.merge(self.router);
+
+        let tcp_listener = TcpListener::bind(self.options.socket_addr).await.unwrap();
+        println!("Binded");
+
+        pin_mut!(tcp_listener);
+        loop {
+            let tower_service = app.clone();
+            let tls_acceptor = tls_acceptor.clone();
+
+            // Wait for new tcp connection
+            let (cnx, addr) = tcp_listener.accept().await.unwrap();
+            println!("TCP Accepted");
+
+            tokio::spawn(async move {
+                // Wait for tls handshake to happen
+                let Ok(stream) = tls_acceptor.accept(cnx).await else {
+                    error!("error during tls handshake connection from {}", addr);
+                    return;
+                };
+
+                println!("TLS Accepted");
+
+                // Hyper has its own `AsyncRead` and `AsyncWrite` traits and doesn't use tokio.
+                // `TokioIo` converts between them.
+                let stream = TokioIo::new(stream);
+
+                // Hyper also has its own `Service` trait and doesn't use tower. We can use
+                // `hyper::service::service_fn` to create a hyper `Service` that calls our app through
+                // `tower::Service::call`.
+                let hyper_service =
+                    hyper::service::service_fn(move |request: Request<Incoming>| {
+                        // We have to clone `tower_service` because hyper's `Service` uses `&self` whereas
+                        // tower's `Service` requires `&mut self`.
+                        //
+                        // We don't need to call `poll_ready` since `Router` is always ready.
+                        tower_service.clone().call(request)
+                    });
+
+                let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                    .serve_connection_with_upgrades(stream, hyper_service)
+                    .await;
+
+                if let Err(err) = ret {
+                    warn!("error serving connection from {}: {}", addr, err);
+                }
+            });
+        }
+
+        // axum::serve(listener, router).await.unwrap()
     }
 }
 
@@ -112,4 +189,27 @@ impl TlsServer {
 
     //     Self { config }
     // }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{routing::post, Router};
+
+    #[tokio::test]
+    async fn test() {
+        let router = Router::new().route("/", post(handler));
+        let options = Options::builder()
+            .disable_redirect()
+            .socket_addr(([127, 0, 0, 1], 8080))
+            .build();
+
+        let server = WebhookServer::new(router, options);
+        server.run().await
+    }
+
+    async fn handler() -> &'static str {
+        println!("Test");
+        "Ok"
+    }
 }
