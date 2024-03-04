13f5816554ee9a44f3f3a31b04b9d80757b2d9d9
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-03-04 16:54:42 +0100

    Add more doc comments for constants

--- stackable-certs/src/ca/consts.rs
+++ stackable-certs/src/ca/consts.rs
@@ -1,8 +1,15 @@
 use const_format::concatcp;
 
+/// The root CA common name DN `CN=Stackable Root CA`.
 pub const ROOT_CA_COMMON_NAME_DN: &str = "CN=Stackable Root CA";
+
+/// A common organization DN `O=Stackable GmbH`.
 pub const ORGANIZATION_DN: &str = "O=Stackable GmbH";
+
+/// The default CA validity time span of one hour (3600 seconds).
 pub const DEFAULT_CA_VALIDITY: u64 = 60 * 60;
+
+/// A common country DN `C=DE`.
 pub const COUNTRY_DN: &str = "C=DE";
 
 /// The root CA subject name containing the common name, organization name and
