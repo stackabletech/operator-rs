76307b78a744e3a0c8a423f27c1e04870c7ab3fe
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-04-02 10:57:25 +0200

    Swap out trace layer with our own

--- crates/stackable-webhook/src/lib.rs
+++ crates/stackable-webhook/src/lib.rs
@@ -23,11 +23,12 @@
 //! enable complete controll over these details if needed.
 //!
 //! [1]: crate::servers::ConversionWebhookServer
-use axum::{body::Body, http::Request, Router};
+use axum::Router;
 use snafu::{ResultExt, Snafu};
+use stackable_telemetry::layer::TraceLayer;
 use tower::ServiceBuilder;
-use tower_http::trace::TraceLayer;
-use tracing::{debug, debug_span, instrument};
+// use tower_http::trace::TraceLayer;
+use tracing::{debug, instrument};
 
 use crate::tls::TlsServer;
 
@@ -136,21 +137,19 @@ impl WebhookServer {
     pub async fn run(self) -> Result<()> {
         debug!("run webhook server");
 
-        // TODO (@Techassi): Switch out for Otel compatible tracing
-        // https://github.com/davidB/tracing-opentelemetry-instrumentation-sdk
-
-        // Create a high-level tracing layer
+        // Create a OpenTelemetry tracing layer
         debug!("create tracing service (layer)");
-        let layer = TraceLayer::new_for_http()
-            .make_span_with(|_: &Request<Body>| debug_span!("webhook_request"))
-            .on_body_chunk(())
-            .on_eos(());
+        let trace_layer = TraceLayer::new().with_opt_in();
 
-        let service = ServiceBuilder::new().layer(layer);
+        // Use a service builder to provide multiple layers at once. Recommended
+        // by the Axum project.
+        //
+        // See https://docs.rs/axum/latest/axum/middleware/index.html#applying-multiple-middleware
+        let service_builder = ServiceBuilder::new().layer(trace_layer);
 
         // Create the root router and merge the provided router into it.
         debug!("create core router and merge provided router");
-        let mut router = Router::new().layer(service);
+        let mut router = Router::new().layer(service_builder);
         router = router.merge(self.router);
 
         // Create server for TLS termination
