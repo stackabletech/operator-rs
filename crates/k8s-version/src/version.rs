2586e77af4e4d4c3accfd64befd75b3e02072004
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-04-22 13:44:02 +0200

    Fix k8s-version unit tests

--- crates/k8s-version/src/version.rs
+++ crates/k8s-version/src/version.rs
@@ -147,10 +147,10 @@ mod test {
 
     #[cfg(feature = "darling")]
     #[rstest]
-    #[case(quote!("v1alpha12"), Version { major: 1, level: Some(Level::Alpha(12)) })]
-    #[case(quote!("v1alpha1"), Version { major: 1, level: Some(Level::Alpha(1)) })]
-    #[case(quote!("v1beta1"), Version { major: 1, level: Some(Level::Beta(1)) })]
-    #[case(quote!("v1"), Version { major: 1, level: None })]
+    #[case(quote!(ignore = "v1alpha12"), Version { major: 1, level: Some(Level::Alpha(12)) })]
+    #[case(quote!(ignore = "v1alpha1"), Version { major: 1, level: Some(Level::Alpha(1)) })]
+    #[case(quote!(ignore = "v1beta1"), Version { major: 1, level: Some(Level::Beta(1)) })]
+    #[case(quote!(ignore = "v1"), Version { major: 1, level: None })]
     fn from_meta(#[case] input: proc_macro2::TokenStream, #[case] expected: Version) {
         let meta = parse_meta(input).expect("valid attribute tokens");
         let version = Version::from_meta(&meta).expect("version must parse from attribute");
