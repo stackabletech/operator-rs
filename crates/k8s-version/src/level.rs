3fa20f4ca3ec9977d74e06dd1069d3f69e8f84f1
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-04-29 15:03:31 +0200

    Add FromMeta unit test for Level

--- crates/k8s-version/src/level.rs
+++ crates/k8s-version/src/level.rs
@@ -159,6 +159,15 @@ mod test {
 
     use super::*;
 
+    #[cfg(feature = "darling")]
+    use quote::quote;
+
+    #[cfg(feature = "darling")]
+    fn parse_meta(tokens: proc_macro2::TokenStream) -> ::std::result::Result<syn::Meta, String> {
+        let attribute: syn::Attribute = syn::parse_quote!(#[#tokens]);
+        Ok(attribute.meta)
+    }
+
     #[template]
     #[rstest]
     #[case(Level::Beta(1), Level::Alpha(1), Ordering::Greater)]
@@ -179,5 +188,16 @@ mod test {
     #[apply(ord_cases)]
     fn partial_ord(input: Level, other: Level, expected: Ordering) {
         assert_eq!(input.partial_cmp(&other), Some(expected))
+    }
+
+    #[cfg(feature = "darling")]
+    #[rstest]
+    #[case(quote!(ignore = "alpha12"), Level::Alpha(12))]
+    #[case(quote!(ignore = "alpha1"), Level::Alpha(1))]
+    #[case(quote!(ignore = "beta1"), Level::Beta(1))]
+    fn from_meta(#[case] input: proc_macro2::TokenStream, #[case] expected: Level) {
+        let meta = parse_meta(input).expect("valid attribute tokens");
+        let version = Level::from_meta(&meta).expect("level must parse from attribute");
+        assert_eq!(version, expected);
     }
 }
