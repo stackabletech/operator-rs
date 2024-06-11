35407685af41ac67952de61b96a742b15e659a80
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2024-06-11 07:23:37 +0200

    test: Improve env var tests
    Originally implemented in and imported from https://github.com/stackabletech/config-utils

--- crates/config-utils/tests/templating.rs
+++ crates/config-utils/tests/templating.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs::{self},
-    path::PathBuf,
-};
+use std::{env, fs, path::PathBuf};
 
 use rstest::rstest;
 
@@ -9,6 +6,8 @@ use config_filler::templating::template;
 
 #[rstest]
 fn test_file_templating(#[files("tests/resources/**/*.in")] test_file_in: PathBuf) {
+    set_example_envs();
+
     let test_file = test_file_in.with_extension("");
     let test_file_expected = test_file_in.with_extension("expected");
 
@@ -19,4 +18,10 @@ fn test_file_templating(#[files("tests/resources/**/*.in")] test_file_in: PathBu
     let expected = fs::read_to_string(&test_file_expected).unwrap();
 
     similar_asserts::assert_eq!(actual, expected);
+}
+
+fn set_example_envs() {
+    // SAFETY: We only use a single thread to set this env vars
+    env::set_var("ENV_TEST", "foo");
+    env::set_var("ENV_TEST_PASSWORD", "admin-pw= withSpace$%\" &&} §");
 }
