c6d6311f93d7018548d0fe51288fa1719d3b8fdf
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2024-06-11 08:41:35 +0200

    test: Test reading from file
    Originally implemented in and imported from https://github.com/stackabletech/config-utils

--- crates/config-utils/tests/templating.rs
+++ crates/config-utils/tests/templating.rs
@@ -1,12 +1,19 @@
-use std::{env, fs, path::PathBuf};
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
 use rstest::rstest;
 
 use config_utils::templating::template;
+use tempfile::tempdir;
 
 #[rstest]
 fn test_file_templating(#[files("tests/resources/**/*.in")] test_file_in: PathBuf) {
-    set_example_envs();
+    let example_dir = create_example_files();
+    set_example_envs(&example_dir);
 
     let test_file = test_file_in.with_extension("");
     let test_file_expected = test_file_in.with_extension("expected");
@@ -20,8 +27,23 @@ fn test_file_templating(#[files("tests/resources/**/*.in")] test_file_in: PathBu
     similar_asserts::assert_eq!(actual, expected);
 }
 
-fn set_example_envs() {
+fn set_example_envs(example_dir: &PathBuf) {
     // SAFETY: We only use a single thread to set this env vars
     env::set_var("ENV_TEST", "foo");
+    env::set_var("ENV_TEST_USERNAME", "example user");
     env::set_var("ENV_TEST_PASSWORD", "admin-pw= withSpace$%\"' &&} §");
+    env::set_var("ENV_TEST_PASSWORD_ENV_NAME", "ENV_TEST_PASSWORD");
+
+    env::set_var("FILE_TEST_42_FILE", example_dir.join("42"));
+    env::set_var("FILE_TEST_42_FILE_ENV_NAME", "FILE_TEST_42_FILE");
+}
+
+/// Returns the directoy where the files reside
+fn create_example_files() -> PathBuf {
+    let dir = tempdir().expect("Failed to create temp dir").into_path();
+
+    let mut file = File::create(dir.join("42")).unwrap();
+    file.write_all(b"42").unwrap();
+
+    dir
 }
