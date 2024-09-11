0111a5c730298cb2711c8f72cf3ac8645928149a
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-09-11 16:52:52 +0200

    docs(stackable-versioned): Update usage guide

--- crates/stackable-versioned/src/lib.rs
+++ crates/stackable-versioned/src/lib.rs
@@ -3,30 +3,6 @@
 //! data type. This will be extended to support SemVer versions, as well as
 //! custom version formats in the future.
 //!
-//! ## Usage Guide
-//!
-//! ```
-//! use stackable_versioned::versioned;
-//!
-//! #[versioned(
-//!     version(name = "v1alpha1"),
-//!     version(name = "v1beta1"),
-//!     version(name = "v1"),
-//!     version(name = "v2"),
-//!     version(name = "v3")
-//! )]
-//! struct Foo {
-//!     /// My docs
-//!     #[versioned(
-//!         added(since = "v1beta1"),
-//!         changed(since = "v1", from_name = "gau"),
-//!         deprecated(since = "v2", note = "not empty")
-//!     )]
-//!     deprecated_bar: usize,
-//!     baz: bool,
-//! }
-//! ```
-//!
 //! See [`versioned`] for an in-depth usage guide and a list of supported
 //! parameters.
 
