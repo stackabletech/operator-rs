d44336e886fe22afa251d21dd7a54c9e5fb1a849
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-09-12 14:25:20 +0200

    docs: Update top-level crate docs

--- crates/stackable-versioned/src/lib.rs
+++ crates/stackable-versioned/src/lib.rs
@@ -1,7 +1,13 @@
-//! This crate enables versioning of structs (and enums in the future). It
-//! currently supports Kubernetes API versions while declaring versions on a
-//! data type. This will be extended to support SemVer versions, as well as
-//! custom version formats in the future.
+//! This crate enables versioning of structs and enums through procedural
+//! macros.
+//!
+//! Currently supported versioning schemes:
+//!
+//! - Kubernetes API versions (eg: `v1alpha1`, `v1beta1`, `v1`, `v2`), with
+//!   optional support for generating CRDs.
+//!
+//! Support will be extended to SemVer versions, as well as custom version
+//! formats in the future.
 //!
 //! See [`versioned`] for an in-depth usage guide and a list of supported
 //! parameters.
