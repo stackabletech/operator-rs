2e9f994da34c541a58496e00c68388e1afe1aa71
Andrew Kenworthy <1712947+adwk67@users.noreply.github.com>
2024-09-30 09:16:41 +0200

    Update crates/stackable-operator/src/commons/authentication/kerberos.rs
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- crates/stackable-operator/src/commons/authentication/kerberos.rs
+++ crates/stackable-operator/src/commons/authentication/kerberos.rs
@@ -7,6 +7,5 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase")]
 pub struct AuthenticationProvider {
     /// Mandatory SecretClass used to obtain keytabs.
-    #[serde(default)]
     pub kerberos_secret_class: String,
 }
