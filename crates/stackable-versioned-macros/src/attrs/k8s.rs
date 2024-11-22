e2eed778e255247620a5805a27fd394a06b9bc19
Techassi <sascha.lautenschlaeger@stackable.tech>
2024-11-22 13:02:36 +0100

    chore: Add missing doc comments for supported K8s arguments

--- crates/stackable-versioned-macros/src/attrs/k8s.rs
+++ crates/stackable-versioned-macros/src/attrs/k8s.rs
@@ -2,11 +2,17 @@ use darling::{util::Flag, FromMeta};
 
 /// This struct contains supported Kubernetes arguments.
 ///
+/// The arguments are passed through to the `#[kube]` attribute. More details can be found in the
+/// official docs: <https://docs.rs/kube/latest/kube/derive.CustomResource.html>.
+///
 /// Supported arguments are:
 ///
 /// - `skip`, which controls skipping parts of the generation.
+/// - `singular`, to specify the singular name of the CR object.
+/// - `plural`, to specify the plural name of the CR object.
 /// - `kind`, which allows overwriting the kind field of the CRD. This defaults to the struct name
 ///    (without the 'Spec' suffix).
+/// - `namespaced`, to specify that this is a namespaced resource rather than cluster level.
 /// - `group`, which sets the CRD group, usually the domain of the company.
 #[derive(Clone, Debug, FromMeta)]
 pub(crate) struct KubernetesArguments {
