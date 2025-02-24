5f24d42c007cbc99df6c5eec73878e0eacc09dec
Techassi <git@techassi.dev>
2025-02-24 17:27:19 +0100

    feat(stackable-versioned): Add convert_with arg in changed action

---
+++ crates/stackable-versioned-macros/fixtures/inputs/default/convert_with.rs
@@ -0,0 +1,12 @@
+#[versioned(version(name = "v1alpha1"), version(name = "v1"), version(name = "v2"))]
+// ---
+struct Foo {
+    #[versioned(
+        // This tests two additional things:
+        // - that both unquoted and quoted usage works
+        // - that the renamed name does get picked up correctly by the conversion function
+        changed(since = "v1", from_type = "u16", from_name = "bar", convert_with = u16_to_u32),
+        changed(since = "v2", from_type = "u32", convert_with = "u32_to_u64")
+    )]
+    baz: u64,
+}
