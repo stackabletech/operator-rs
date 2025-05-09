44b53988ec2c97d255eada81b46f0240efef8813
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-05-09 11:02:38 +0200

    docs

--- crates/stackable-crd-previewer/src/lib.rs
+++ crates/stackable-crd-previewer/src/lib.rs
@@ -1,1 +1,4 @@
-
+// We need *something* for this to count as workspace member.
+//
+// This needs to be a separate workspace member, as can not add a build.rs to stackable-operator,
+// that depends on stackable-operator structs.
