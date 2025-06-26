55f06b40845deea52314c1fcb69842de33132761
Techassi <git@techassi.dev>
2025-06-26 12:34:26 +0200

    test(stackable-versioned): Simplify merged_crd test

--- crates/stackable-versioned/tests/merged_crd.rs
+++ crates/stackable-versioned/tests/merged_crd.rs
@@ -4,37 +4,19 @@ mod person;
 
 #[test]
 fn stored_apiversion() {
-    let desired_stored_apiversion = PersonVersion::V2;
+    let stored_apiversion = PersonVersion::V2;
 
-    let merged_crd =
-        Person::merged_crd(desired_stored_apiversion).expect("the CRDs must be mergeable");
+    let merged_crd = Person::merged_crd(stored_apiversion).expect("the CRDs must be mergeable");
 
-    // First, we ensure that all storage fields have the correct value.
-    let all_storage_fields_correct =
-        merged_crd
-            .spec
-            .versions
-            .iter()
-            .enumerate()
-            .all(
-                |(idx, crd)| {
-                    if idx == 0 { crd.storage } else { !crd.storage }
-                },
-            );
-
-    assert!(all_storage_fields_correct);
-
-    // Lastly, we ensure the first version (which is always the stored version)
-    // is the one we expect.
-    let stored_apiversion = &merged_crd
+    // We ensure that the merged CRD contains at least one version marked as
+    // storage = true.
+    let crd = merged_crd
         .spec
         .versions
-        .first()
-        .expect("there must be at least one CRD version")
-        .name;
+        .iter()
+        .find(|crd| crd.storage)
+        .expect("The merged CRD must contain at least one version marked with storage = true");
 
-    assert_eq!(
-        stored_apiversion,
-        desired_stored_apiversion.as_version_str()
-    );
+    // This asserts that the name (version) of the CRD matches the one we expect
+    assert_eq!(crd.name, stored_apiversion.as_version_str());
 }
