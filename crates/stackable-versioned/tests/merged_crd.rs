6e8621701334434fac19e23de3dd9b28c4405f7e
Techassi <git@techassi.dev>
2025-06-26 11:56:24 +0200

    test(stackable-versioned): Improve merged_crd integration test

--- crates/stackable-versioned/tests/merged_crd.rs
+++ crates/stackable-versioned/tests/merged_crd.rs
@@ -4,8 +4,12 @@ mod person;
 
 #[test]
 fn stored_apiversion() {
-    let merged_crd = Person::merged_crd(PersonVersion::V2).expect("the CRDs must be mergeable");
+    let desired_stored_apiversion = PersonVersion::V2;
 
+    let merged_crd =
+        Person::merged_crd(desired_stored_apiversion).expect("the CRDs must be mergeable");
+
+    // First, we ensure that all storage fields have the correct value.
     let all_storage_fields_correct =
         merged_crd
             .spec
@@ -19,4 +23,18 @@ fn stored_apiversion() {
             );
 
     assert!(all_storage_fields_correct);
+
+    // Lastly, we ensure the first version (which is always the stored version)
+    // is the one we expect.
+    let stored_apiversion = &merged_crd
+        .spec
+        .versions
+        .first()
+        .expect("there must be at least one CRD version")
+        .name;
+
+    assert_eq!(
+        stored_apiversion,
+        desired_stored_apiversion.as_version_str()
+    );
 }
