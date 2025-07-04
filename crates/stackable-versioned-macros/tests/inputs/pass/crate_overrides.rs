26e5565d601adceae7328cdd2288541a6c946fb9
Techassi <git@techassi.dev>
2025-07-04 09:54:34 +0200

    test(stackable-versioned): Adjust test inputs and UI stderr snapshots

---
+++ crates/stackable-versioned-macros/tests/inputs/pass/crate_overrides.rs
@@ -0,0 +1,30 @@
+use stackable_versioned::versioned;
+// ---
+#[versioned(
+    version(name = "v1alpha1"),
+    version(name = "v1beta1"),
+    version(name = "v1"),
+    crates(
+        kube_core = ::kube::core,
+        schemars = ::schemars
+    )
+)]
+// ---
+pub mod versioned {
+    #[versioned(crd(group = "foo.example.org"))]
+    #[derive(
+        Clone,
+        Debug,
+        serde::Deserialize,
+        serde::Serialize,
+        schemars::JsonSchema,
+        kube::CustomResource,
+    )]
+    pub struct FooSpec {
+        #[versioned(added(since = "v1beta1"), changed(since = "v1", from_name = "bah"))]
+        bar: usize,
+        baz: bool,
+    }
+}
+// ---
+fn main() {}
