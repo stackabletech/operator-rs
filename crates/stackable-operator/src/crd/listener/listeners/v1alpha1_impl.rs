22b173273f50d800aaa2593d7bfa3d4d186e78d6
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-11-11 12:29:52 +0100

    Add a test for Listener merging

--- crates/stackable-operator/src/crd/listener/listeners/v1alpha1_impl.rs
+++ crates/stackable-operator/src/crd/listener/listeners/v1alpha1_impl.rs
@@ -1,3 +1,5 @@
+use k8s_openapi::{DeepMerge, merge_strategies};
+
 use crate::crd::listener::listeners::v1alpha1::{
     Listener, ListenerIngress, ListenerPort, ListenerSpec, ListenerStatus,
 };
@@ -8,87 +10,139 @@ impl ListenerSpec {
     }
 }
 
-impl k8s_openapi::DeepMerge for Listener {
+impl DeepMerge for Listener {
     fn merge_from(&mut self, other: Self) {
-        k8s_openapi::DeepMerge::merge_from(&mut self.metadata, other.metadata);
-        k8s_openapi::DeepMerge::merge_from(&mut self.spec, other.spec);
-        k8s_openapi::DeepMerge::merge_from(&mut self.status, other.status);
+        DeepMerge::merge_from(&mut self.metadata, other.metadata);
+        DeepMerge::merge_from(&mut self.spec, other.spec);
+        DeepMerge::merge_from(&mut self.status, other.status);
     }
 }
 
-impl k8s_openapi::DeepMerge for ListenerSpec {
+impl DeepMerge for ListenerSpec {
     fn merge_from(&mut self, other: Self) {
-        k8s_openapi::DeepMerge::merge_from(&mut self.class_name, other.class_name);
-        k8s_openapi::merge_strategies::map::granular(
+        DeepMerge::merge_from(&mut self.class_name, other.class_name);
+        merge_strategies::map::granular(
             &mut self.extra_pod_selector_labels,
             other.extra_pod_selector_labels,
             |current_item, other_item| {
-                k8s_openapi::DeepMerge::merge_from(current_item, other_item);
+                DeepMerge::merge_from(current_item, other_item);
             },
         );
-        k8s_openapi::merge_strategies::list::map(
+        merge_strategies::list::map(
             &mut self.ports,
             other.ports,
             &[|lhs, rhs| lhs.name == rhs.name],
             |current_item, other_item| {
-                k8s_openapi::DeepMerge::merge_from(current_item, other_item);
+                DeepMerge::merge_from(current_item, other_item);
             },
         );
-        k8s_openapi::DeepMerge::merge_from(
+        DeepMerge::merge_from(
             &mut self.publish_not_ready_addresses,
             other.publish_not_ready_addresses,
         );
-        todo!()
     }
 }
 
-impl k8s_openapi::DeepMerge for ListenerStatus {
+impl DeepMerge for ListenerStatus {
     fn merge_from(&mut self, other: Self) {
-        k8s_openapi::DeepMerge::merge_from(&mut self.service_name, other.service_name);
-        k8s_openapi::merge_strategies::list::map(
+        DeepMerge::merge_from(&mut self.service_name, other.service_name);
+        merge_strategies::list::map(
             &mut self.ingress_addresses,
             other.ingress_addresses,
             &[|lhs, rhs| lhs.address == rhs.address],
             |current_item, other_item| {
-                k8s_openapi::DeepMerge::merge_from(current_item, other_item);
+                DeepMerge::merge_from(current_item, other_item);
             },
         );
-        k8s_openapi::merge_strategies::map::granular(
+        merge_strategies::map::granular(
             &mut self.node_ports,
             other.node_ports,
             |current_item, other_item| {
-                k8s_openapi::DeepMerge::merge_from(current_item, other_item);
+                DeepMerge::merge_from(current_item, other_item);
             },
         );
     }
 }
 
-impl k8s_openapi::DeepMerge for ListenerIngress {
+impl DeepMerge for ListenerIngress {
     fn merge_from(&mut self, other: Self) {
-        k8s_openapi::DeepMerge::merge_from(&mut self.address, other.address);
+        DeepMerge::merge_from(&mut self.address, other.address);
         self.address_type = other.address_type;
-        k8s_openapi::merge_strategies::map::granular(
+        merge_strategies::map::granular(
             &mut self.ports,
             other.ports,
             |current_item, other_item| {
-                k8s_openapi::DeepMerge::merge_from(current_item, other_item);
+                DeepMerge::merge_from(current_item, other_item);
             },
         );
     }
 }
 
-impl k8s_openapi::DeepMerge for ListenerPort {
+impl DeepMerge for ListenerPort {
     fn merge_from(&mut self, other: Self) {
-        k8s_openapi::DeepMerge::merge_from(&mut self.name, other.name);
-        k8s_openapi::DeepMerge::merge_from(&mut self.port, other.port);
-        k8s_openapi::DeepMerge::merge_from(&mut self.protocol, other.protocol);
+        DeepMerge::merge_from(&mut self.name, other.name);
+        DeepMerge::merge_from(&mut self.port, other.port);
+        DeepMerge::merge_from(&mut self.protocol, other.protocol);
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn deep_merge_listener() {
-        todo!("Add some basic tests for merging");
+        let mut base: ListenerSpec = serde_yaml::from_str(
+            "
+className: my-listener-class
+extraPodSelectorLabels:
+  foo: bar
+ports:
+  - name: http
+    port: 8080
+    protocol: http
+  - name: https
+    port: 8080
+    protocol: https
+# publishNotReadyAddresses defaults to true
+",
+        )
+        .unwrap();
+
+        let patch: ListenerSpec = serde_yaml::from_str(
+            "
+className: custom-listener-class
+extraPodSelectorLabels:
+  foo: overridden
+  extra: label
+ports:
+  - name: https
+    port: 8443
+publishNotReadyAddresses: false
+",
+        )
+        .unwrap();
+
+        base.merge_from(patch);
+
+        let expected: ListenerSpec = serde_yaml::from_str(
+            "
+className: custom-listener-class
+extraPodSelectorLabels:
+  foo: overridden
+  extra: label
+ports:
+  - name: http
+    port: 8080
+    protocol: http
+  - name: https
+    port: 8443 # overridden
+    protocol: https
+publishNotReadyAddresses: false
+",
+        )
+        .unwrap();
+
+        assert_eq!(base, expected);
     }
 }
