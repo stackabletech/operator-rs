a46cadae07b77ecdedf944df4366b98cfccce5ce
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-11-20 15:46:02 +0100

    Improve CRD docs

--- crates/stackable-operator/src/patchinator/crd.rs
+++ crates/stackable-operator/src/patchinator/crd.rs
@@ -7,7 +7,10 @@ use crate::utils::crds::raw_object_list_schema;
 #[derive(Clone, Debug, Deserialize, JsonSchema, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ObjectOverrides {
-    /// TODO docs
+    /// A list of generic Kubernetes objects, which are merged onto the objects that the operator
+    /// creates.
+    ///
+    // TODO: Add link to concepts page once it exists
     #[serde(default)]
     #[schemars(schema_with = "raw_object_list_schema")]
     pub object_overrides: Vec<DynamicObject>,
