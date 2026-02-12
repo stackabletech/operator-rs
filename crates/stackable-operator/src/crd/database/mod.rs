32c0ec97b590b2ac8b02e5c25b9636534b84a28b
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2026-02-12 10:55:43 +0100

    Remove leftover code

--- crates/stackable-operator/src/crd/database/mod.rs
+++ crates/stackable-operator/src/crd/database/mod.rs
@@ -16,27 +16,3 @@ pub enum Error {
     #[snafu(context(false), display("derby error"))]
     Derby { source: databases::derby::Error },
 }
-
-// /// TODO docs
-// pub trait CeleryDatabaseConnection {
-//     /// TODO docs, e.g. on what are valid characters for unique_database_name
-//     fn celery_connection_details(
-//         &self,
-//         unique_database_name: &str,
-//     ) -> CeleryDatabaseConnectionDetails;
-// }
-
-// pub struct CeleryDatabaseConnectionDetails {
-//     /// The connection URI, which can contain env variable templates, e.g.
-//     /// `redis://:redis@airflow-redis-master:6379/0`
-//     pub uri_template: String,
-
-//     /// The [`EnvVar`]s the operator needs to mount into the created Pods.
-//     pub env_vars: Vec<EnvVar>,
-// }
-
-// impl CeleryDatabaseConnectionDetails {
-//     pub fn add_to_container(&self, cb: &mut ContainerBuilder) {
-//         cb.add_env_vars(self.env_vars.clone());
-//     }
-// }
