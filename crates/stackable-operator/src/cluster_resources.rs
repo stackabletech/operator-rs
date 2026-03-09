1e0cde4e0037dd7729672aab5d27f684d96641af
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2026-03-09 10:30:13 +0100

    refactor: Use cfg!

--- crates/stackable-operator/src/cluster_resources.rs
+++ crates/stackable-operator/src/cluster_resources.rs
@@ -671,11 +671,16 @@ impl<'a> ClusterResources<'a> {
     /// * `client` - The client which is used to access Kubernetes
     pub async fn delete_orphaned_resources(self, client: &Client) -> Result<()> {
         // We can only delete Listeners in case the "crds" feature is enabled, otherwise it's a NOP.
-        #[cfg(feature = "crds")]
-        let delete_listeners = self
-            .delete_orphaned_resources_of_kind::<crate::crd::listener::v1alpha1::Listener>(client);
-        #[cfg(not(feature = "crds"))]
-        let delete_listeners = async { Ok(()) };
+        let delete_listeners = async {
+            if cfg!(feature = "crds") {
+                self.delete_orphaned_resources_of_kind::<crate::crd::listener::v1alpha1::Listener>(
+                    client,
+                )
+                .await
+            } else {
+                Ok(())
+            }
+        };
 
         tokio::try_join!(
             self.delete_orphaned_resources_of_kind::<Service>(client),
