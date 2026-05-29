a3829c436faaa93af0e10583a70f0f9c55577630
Malte Sander <malte.sander.it@gmail.com>
2026-05-29 14:24:25 +0200

    fix: add missing traits; macros and types pub.

--- crates/stackable-operator/src/v2/mod.rs
+++ crates/stackable-operator/src/v2/mod.rs
@@ -1,1 +1,24 @@
+use crate::v2::types::kubernetes::Uid;
+
 pub mod config_overrides;
+pub mod macros;
+pub mod types;
+
+/// Has a non-empty name
+///
+/// Useful as an object reference; Should not be used to create an object because the name could
+/// violate the naming constraints (e.g. maximum length) of the object.
+pub trait HasName {
+    #[allow(dead_code)]
+    fn to_name(&self) -> String;
+}
+
+/// Has a Kubernetes UID
+pub trait HasUid {
+    fn to_uid(&self) -> Uid;
+}
+
+/// The name is a valid label value
+pub trait NameIsValidLabelValue {
+    fn to_label_value(&self) -> String;
+}
