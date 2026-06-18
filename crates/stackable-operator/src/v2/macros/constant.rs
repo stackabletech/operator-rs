365ccb529aa1e6d30faa7c104a169c4f5329d9c2
Siegfried Weber <mail@siegfriedweber.net>
2026-06-18 14:16:11 +0200

    Allow expressions in the constant macro

--- crates/stackable-operator/src/v2/macros/constant.rs
+++ crates/stackable-operator/src/v2/macros/constant.rs
@@ -9,12 +9,17 @@
 ///
 /// use stackable_operator::constant;
 /// use stackable_operator::v2::types::kubernetes::VolumeName;
+/// use stackable_operator::v2::builder::pod::container::EnvVarName;
+///
 /// constant!(DATA_VOLUME_NAME: VolumeName = "data");
 /// constant!(pub CONFIG_VOLUME_NAME: VolumeName = "config");
+///
+/// const CONFIG_OPTION_SECURITY_ENABLED: &str = "SECURITY_ENABLED";
+/// constant!(ENV_VAR_NAME_SECURITY_ENABLED: EnvVarName = CONFIG_OPTION_SECURITY_ENABLED);
 /// ```
 #[macro_export(local_inner_macros)]
 macro_rules! constant {
-    ($qualifier:vis $name:ident: $type:ident = $value:literal) => {
+    ($qualifier:vis $name:ident: $type:ident = $value:expr) => {
         $qualifier static $name: std::sync::LazyLock<$type> =
             std::sync::LazyLock::new(|| $type::from_str($value).expect("should be a valid $name"));
     };
